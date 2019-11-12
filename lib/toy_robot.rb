378a1117fb92445fb98a6b7f88259a6faa652e3a
Eser Poyraz <eserpoyraz@gmail.com>
2019-11-12 15:30:17 +0000

    initial commit

---
+++ lib/toy_robot.rb
@@ -0,0 +1,12 @@
+require "toy_robot/version"
+require "toy_robot/robot"
+require "toy_robot/table"
+require "toy_robot/simulator"
+require "toy_robot/command"
+require "toy_robot/cli"
+
+module ToyRobot
+  class Error < StandardError; end
+
+  # Your code goes here...
+end
